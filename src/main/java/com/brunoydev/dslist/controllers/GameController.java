db251a4662aff703c6e25bef4cc88687172e4230
bruno garcia <brunrgarcia.dev@gmail.com>
2025-08-18 14:08:18 -0300

    Add: GameController

---
+++ src/main/java/com/brunoydev/dslist/controllers/GameController.java
@@ -0,0 +1,26 @@
+package com.brunoydev.dslist.controllers;
+
+import com.brunoydev.dslist.dto.GameMinDTO;
+import com.brunoydev.dslist.entities.Game;
+import com.brunoydev.dslist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<GameMinDTO> findAll(){
+
+        return gameService.findAll();
+    }
+
+}
