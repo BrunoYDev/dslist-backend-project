56dbe9e0cd1fcad70f135b00b45118ddd35bc2bc
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2025-08-19 15:34:34 -0300

    Add: findById in Game service and controller

--- src/main/java/com/brunoydev/dslist/controllers/GameController.java
+++ src/main/java/com/brunoydev/dslist/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.brunoydev.dslist.controllers;
 
+import com.brunoydev.dslist.dto.GameDTO;
 import com.brunoydev.dslist.dto.GameMinDTO;
 import com.brunoydev.dslist.entities.Game;
 import com.brunoydev.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +18,12 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{gameId}")
+    public GameDTO findById(@PathVariable Long gameId){
+
+        return gameService.findById(gameId);
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll(){
--- src/main/java/com/brunoydev/dslist/services/GameService.java
+++ src/main/java/com/brunoydev/dslist/services/GameService.java
@@ -1,12 +1,15 @@
 package com.brunoydev.dslist.services;
 
+import com.brunoydev.dslist.dto.GameDTO;
 import com.brunoydev.dslist.dto.GameMinDTO;
 import com.brunoydev.dslist.entities.Game;
 import com.brunoydev.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class GameService {
@@ -14,10 +17,18 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
 
         // Can override lambda expression with GameMinDTO::new
         return result.stream().map(game -> new GameMinDTO(game)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long gameId){
+        Game result = gameRepository.findById(gameId).get();
+
+        return new GameDTO(result);
     }
 }
